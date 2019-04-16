9b36f962af0282983283be6418d0c546ac32fd35
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-04-16 22:43:43 +0300

    Add Selenium demo test.

---
+++ backend/src/integrationTest/java/com/acme/dbo/ui/SeleniumIT.java
@@ -0,0 +1,48 @@
+package com.acme.dbo.ui;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Ignore
+public class SeleniumIT {
+    @LocalServerPort protected int serverPort;
+
+    @Test
+    public void shouldRespondWithSwaggerUI() throws InterruptedException {
+        //download at https://chromedriver.storage.googleapis.com/index.html?path=74.0.3729.6/
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        ChromeOptions chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("http://localhost:" + serverPort + "/swagger-ui.html");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("base-url")));
+        driver.findElement(By.xpath("//a[@href='#/account-controller']")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='#/operations/account-controller/getAccountsUsingGET']")));
+        driver.findElement(By.xpath("//a[@href='#/operations/account-controller/getAccountsUsingGET']")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='operations-account-controller-getAccountsUsingGET']/div[2]/div/div[2]/div[1]/div[2]/button")));
+        driver.findElement(By.xpath("//*[@id='operations-account-controller-getAccountsUsingGET']/div[2]/div/div[2]/div[1]/div[2]/button")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='operations-account-controller-getAccountsUsingGET']/div[2]/div/div[3]/button")));
+        driver.findElement(By.xpath("//*[@id='operations-account-controller-getAccountsUsingGET']/div[2]/div/div[3]/button")).click();
+
+        driver.quit();
+    }
+
+}
