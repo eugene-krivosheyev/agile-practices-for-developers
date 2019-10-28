97d18f9b4615f8e4898e7034509819407052a985
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-10-28 14:43:52 +0300

    Try Chrome headless with updated DriverManager.

--- src/test/java/com/acme/dbo/config/TestConfig.java
+++ src/test/java/com/acme/dbo/config/TestConfig.java
@@ -2,7 +2,8 @@ package com.acme.dbo.config;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
@@ -13,7 +14,10 @@ public class TestConfig {
     @Lazy @Bean
     @Profile("it")
     public WebDriver webDriver() {
-        WebDriverManager.phantomjs().setup();
-        return new PhantomJSDriver();
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver(new ChromeOptions()
+                .setHeadless(true)
+                .addArguments("--headless")
+                .addArguments("--disable-gpu"));
     }
 }
